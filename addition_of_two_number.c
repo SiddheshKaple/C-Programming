02c0585a22c92dbaef39e01f27f2e394c120345e
SiddheshKaple <63679648+SiddheshKaple@users.noreply.github.com>
2020-04-15 03:26:54 +0530

    Program to add a and b only if value of a is even number.

---
+++ addition_of_two_number.c
@@ -0,0 +1,18 @@
+/// Program to add a and b only if value for a is even
+
+#include<stdio.h>
+void main()
+{
+    int a=5;
+    int b=6;
+    // Check if a if even or not
+    // if true
+    if( a % 2 == 0)
+    {
+        printf("Value of a is even and result is : %d", (a+b));
+    }
+    //if false
+    else{
+        printf("Value of a is not even");
+    }
+}
